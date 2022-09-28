4df25896fcc30db61f5d2ac433e16c35d12fbdd8
Lindsey <lindseykaycarlson@outlook.com>
2022-09-28 16:39:26 -0700

    Conference Create & Attendee Create views

--- attendees/api_views.py
+++ attendees/api_views.py
@@ -1,14 +1,16 @@
 from django.http import JsonResponse
 from common.json import ModelEncoder
 from .models import Attendee
+from events.models import Conference
 from events.api_views import ConferenceListEncoder
-
+from django.views.decorators.http import require_http_methods
+import json
 
 class AttendeeListEncoder(ModelEncoder):
     model = Attendee
     properties = ["name"]
 
-
+@require_http_methods(["GET", "POST"])
 def api_list_attendees(request, conference_id):
     """
     Lists the attendees names and the link to the attendee
@@ -29,8 +31,20 @@ def api_list_attendees(request, conference_id):
         ]
     }
     """
-    attendees = Attendee.objects.filter(conference=conference_id)
-    return JsonResponse({"attendees": attendees}, encoder=AttendeeListEncoder)
+    if request.method == "GET":
+        attendees = Attendee.objects.filter(conference=conference_id)
+        return JsonResponse({"attendees": attendees}, encoder=AttendeeListEncoder)
+    else:
+        content = json.loads(request.body)
+        try:
+            conference = Conference.objects.get(id=conference_id)
+            content["conference"]=conference
+        except Conference.DoesNotExist:
+            return JsonResponse({"message": "Invalid conference id"}, status=400)
+
+        attendee = Attendee.objects.create(**content)
+        return JsonResponse(attendee, encoder=AttendeeDetailEncoder, safe=False)
+
 
 
 class AttendeeDetailEncoder(ModelEncoder):
--- events/api_views.py
+++ events/api_views.py
@@ -11,6 +11,7 @@ class ConferenceListEncoder(ModelEncoder):
     properties = ["name"]
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
     """
     Lists the conference names and the link to the conference.
@@ -30,10 +31,22 @@ def api_list_conferences(request):
         ]
     }
     """
-    conferences = Conference.objects.all()
-    return JsonResponse(
-        {"conferences": conferences}, encoder=ConferenceListEncoder
-    )
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences}, encoder=ConferenceListEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse({"message": "Invalid location id"}, status=400)
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference, encoder=ConferenceDetailEncoder, safe=False
+        )
 
 
 class LocationListEncoder(ModelEncoder):
